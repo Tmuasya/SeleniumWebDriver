347ed8b68a18ef2e5c84d7cfff3a22403bf783f5
martinwairegi <muasya65@gmail.com>
2022-11-15 15:43:57 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\DM\\Documents\\Localhost\\Webdriver\\Webdriver\\src\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com");
+        System.out.println(driver.getTitle());
+        Thread.sleep(1000);
+        driver.quit(); 
+    }
+}
